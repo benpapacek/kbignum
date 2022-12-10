8bfc0d4781be93464ca1ebdb5693018f69dd64d7
Ben Papacek <benpapacek@gmail.com>
2022-12-10 15:21:37 +1100

    Add constants to KMathContext

--- kbignum/src/commonMain/kotlin/com/papacekb/kbignum/KMathContext.kt
+++ kbignum/src/commonMain/kotlin/com/papacekb/kbignum/KMathContext.kt
@@ -1,3 +1,12 @@
 package com.papacekb.kbignum
 
-data class KMathContext(val precision: Int, val kRoundingMode: KRoundingMode = KRoundingMode.HALF_UP)
+data class KMathContext(val precision: Int, val kRoundingMode: KRoundingMode = KRoundingMode.HALF_UP) {
+
+    companion object {
+        val UNLIMITED = KMathContext(0, KRoundingMode.HALF_UP)
+        val DECIMAL32 = KMathContext(7, KRoundingMode.HALF_EVEN)
+        val DECIMAL64 = KMathContext(16, KRoundingMode.HALF_EVEN)
+        val DECIMAL128 = KMathContext(34, KRoundingMode.HALF_EVEN)
+    }
+
+}
